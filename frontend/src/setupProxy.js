33192f10d429df7911844f9217b7df5396663e49
bugcruher07 <tarun.develops@gmail.com>
2025-08-05 15:00:04 +0530

    changing title

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,11 +1,18 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
+    // app.use(
+    //     '/api',
+    //     createProxyMiddleware({
+    //         target: 'https://ai-grub-hub-ts-production.up.railway.app',
+    //         changeOrigin: true,
+    //     })
+    // );
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'https://ai-grub-hub-ts-production.up.railway.app',
-            changeOrigin: true,
+            target:'http://localhost:5000',
+            changeOrigin:true,
         })
-    );
+    )
 };
